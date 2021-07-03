a02417b62389ae231a7a18339bbd553019b49b71
alfapappu <diploma.dsd@gmail.com>
2021-07-03 22:26:18 +0530

    first app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+
+const App = () => {
+
+  let date = new Date().toLocaleTimeString()
+  const [init, newVal] = useState(date)
+
+  setInterval(() => {
+    date = new Date().toLocaleTimeString()
+    newVal(date)
+  }, 1000);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>{init}</h1>
+    </>
   );
 }
 
